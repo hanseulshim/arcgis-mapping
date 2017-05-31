b2c0389bbd8136eccaf3c6f451b40588780bf815
hanseulshim <hanseulshim@gmail.com>
2017-05-31 17:48:56 -0500

    initial commit

--- src/reducers/map.js
+++ src/reducers/map.js
@@ -1,5 +1,6 @@
 import MapView from 'esri/views/MapView';
 import EsriMap from 'esri/Map';
+import FeatureLayer from 'esri/layers/FeatureLayer'
 
 const map = (state = { }, action) => {
 
@@ -9,7 +10,20 @@ const map = (state = { }, action) => {
       return {
         mapCtrl: new MapView({
           container: action.domNode,
-          map: new EsriMap({basemap: 'topo'})
+          map: new EsriMap({
+            basemap: 'hybrid',
+            layers: [new FeatureLayer({
+              url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/ArcGIS/rest/services/Apple_Stores/FeatureServer/0"
+            })]
+          }),
+          extent: { // autocasts as new Extent()
+            xmin: -17570980.1213845,
+            ymin: -4581917.34654093,
+            xmax: 17074834.7168934,
+            ymax: 8675820.74498175,
+            spatialReference: 102100
+          }
+
         })
       }
 
